d8d767f2f2969ac948eb468a9c6753c2c3805b2b
SarahAlkhubaizy <sarahalkhubaizy@gmail.com>
2026-09-07 23:47:30 +0300

    find

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -4,4 +4,3 @@
 def schools_by_topic(mongo_collection, topic):
         """Returns the list of schools having a specific topic"""
         return list(mongo_collection.find({"topics": topic}))
-    
