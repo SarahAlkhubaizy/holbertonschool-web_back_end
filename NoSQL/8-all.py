2101752fc3e319069b1ecd9127432d41f124eaab
SarahAlkhubaizy <sarahalkhubaizy@gmail.com>
2026-09-07 23:18:45 +0300

    Find and insert

---
+++ NoSQL/8-all.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""List all documents in a collection"""
+
+def list_all(mongo_collection):
+    """Lists all documents in a collection"""
+    return list(mongo_collection.find())
